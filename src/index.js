27cb73a2a652da32a62b63161911a9d8c50e3a3d
nivruttipatil549 <nivruttipatil549@gmail.com>
2022-02-23 17:58:45 +0530

    Assignment 2

--- src/index.js
+++ src/index.js
@@ -10,3 +10,7 @@ app.use('/', route);
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+
+
+
+//today example23feb2022
--- src/routes/route.js
+++ src/routes/route.js
@@ -9,5 +9,64 @@ router.get('/students/:name', function(req, res) {
 
 
 
+router.get('/movies',function (req,res){
+    res.send('["Dangal","Sanju","Pk","ABCD","Rockstar"]')
+});
+
+router.get('/movies/:moviesID',function(req,res){
+    mov=["Dangal","Sanju","Pk","ABCD","Rockstar"]
+    let value=req.params.moviesID;
+    if (value>mov.length-1){
+        res.send('"doesnot exist"')
+    }else{
+        res.send(mov[value])
+    }
+});
+
+router.get('/moviesz',function (req,res){
+    res.send([{
+        id: 1,
+        name: 'The Shining'
+       }, {
+        id: 2,
+        name: 'Incendies'
+       }, {
+        id: 3,
+        name: 'Rang de Basanti'
+       }, {
+        id: 4,
+        name: 'Finding Demo'
+       }]
+       )
+});
+
+
+router.get('/films/:filmID',function (req,res){
+    let movi=[{
+        id: 1,
+        name: 'The Shining'
+       }, {
+        id: 2,
+        name: 'Incendies'
+       }, {
+        id: 3,
+        name: 'Rang de Basanti'
+       }, {
+        id: 4,
+        name: 'Finding Demo'
+       }]
+       let value =req.params.filmID;
+       let found=false;
+       for(i=0;i<movi.length;i++){
+           if(movi[i].id==value){
+               found=true;
+               res.send(movi[i])
+               break
+           }
+       }
+if (found==false){
+    res.send('No movie exists with this id')
+}
+});
 
 module.exports = router;
