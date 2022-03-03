9dab08fb4f94793242cc9d3ca03d1cd7e3f13f6a
nivruttipatil549 <nivruttipatil549@gmail.com>
2022-03-03 08:50:46 +0530

    done

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://nivruttipatil549:hhFh5IflfV7AB6Ay@cluster0.k5wal.mongodb.net/Nivrutti549-DB?authSource=admin&replicaSet=atlas-1hk4p4-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
